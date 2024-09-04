ee3e66f6356a3e265a34abe689b55803d3dbdada
Esurio <esurio@esurio1673.net>
2024-09-05 00:06:17 +0900

    show mkctl version

--- cmd/root.go
+++ cmd/root.go
@@ -7,10 +7,14 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var version = "0.0.1"
+
 var rootCmd = &cobra.Command{
 	Use:   "mkctl",
 	Short: "CLI tool for managing misskey",
-	Run:   func(cmd *cobra.Command, args []string) {},
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("mkctl version: %s\n", version)
+	},
 }
 
 func Execute() {
