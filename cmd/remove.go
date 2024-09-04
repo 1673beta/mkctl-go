ff8f6a6a028c0a3ec197feb0e6ca1c1c3c897dca
Esurio <esurio@esurio1673.net>
2024-09-04 23:45:04 +0900

    removeコマンドの構文を変更

--- cmd/remove.go
+++ cmd/remove.go
@@ -2,13 +2,12 @@ package cmd
 
 import (
 	"fmt"
+	"mkctl/cmd/util"
 	"os"
 	"strconv"
 	"time"
 
 	"github.com/spf13/cobra"
-
-	"mkctl/cmd/util"
 )
 
 var deleteDays int
@@ -16,7 +15,12 @@ var remoteOnly bool
 
 var removeCmd = &cobra.Command{
 	Use:   "remove",
-	Short: "Remove old posts from database.",
+	Short: "Remove from database.",
+}
+
+var notesCmd = &cobra.Command{
+	Use:   "notes",
+	Short: "Remove old notes from database.",
 	Run: func(cmd *cobra.Command, args []string) {
 		cutoff := time.Now().AddDate(0, 0, -deleteDays)
 		ms := cutoff.Sub(time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC)).Milliseconds()
@@ -25,23 +29,23 @@ var removeCmd = &cobra.Command{
 
 		db, err := util.ConnectToDb()
 		if err != nil {
-			fmt.Printf("error while connecting to database: %v", err)
+			fmt.Printf("Error while connecting to database: %v\n", err)
 			os.Exit(1)
 		}
 		defer db.Close()
 
 		if remoteOnly {
-			query := fmt.Sprintf(`DELETE FROM note WHERE SUBSTRING(note."id", 1, 8) <= '%s' AND note."userHost" IS NOT NULL`, encoded)
+			query := fmt.Sprintf(`DELETE FROM note WHERE SUBSTRING(note."id", 1, 8) <= %s AND note."userHost" IS NOT NULL`, encoded)
 			_, err := db.Exec(query)
 			if err != nil {
-				fmt.Printf("error while deleting old notes: %v", err)
+				fmt.Printf("Error while deleting notes: %v\n", err)
 				os.Exit(1)
 			}
 		} else {
-			query := fmt.Sprintf(`DELETE FROM note WHERE SUBSTRING(note."id", 1, 8) <= '%s`, encoded)
+			query := fmt.Sprintf(`DELETE FROM note WHERE SUBSTRING(note."id", 1, 8) <= %s`, encoded)
 			_, err := db.Exec(query)
 			if err != nil {
-				fmt.Printf("error while deleting old notes: %v", err)
+				fmt.Printf("Error while deleting notes: %v\n", err)
 				os.Exit(1)
 			}
 		}
@@ -51,5 +55,6 @@ var removeCmd = &cobra.Command{
 func init() {
 	removeCmd.Flags().IntVarP(&deleteDays, "days", "d", 120, "How old notes have to be before they are deleted. Defaults to 120.")
 	removeCmd.Flags().BoolVarP(&remoteOnly, "remote", "r", false, "Only delete notes from remote server.")
+	removeCmd.AddCommand(notesCmd)
 	rootCmd.AddCommand(removeCmd)
 }
